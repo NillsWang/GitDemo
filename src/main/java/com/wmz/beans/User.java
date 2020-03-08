e3823dae6e0fe7b9def5649eb5b8fc39cdfc208a
eclipse_user <1404963301@qq.com>
2020-03-08 23:30:28 +0800

    second commit

--- src/main/java/com/wmz/beans/User.java
+++ src/main/java/com/wmz/beans/User.java
@@ -2,6 +2,18 @@ package com.wmz.beans;
 
 public class User {
 	private String userName;
-	
+	private Integer num;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
 
 }
